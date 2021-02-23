fc41aa4769c5f33846fa6ace48d264838e23bd0b
pedr0luiz <pedroluiz@gmail.com>
2021-02-23 12:33:00 -0300

    colisão raquete com bola

---
+++ Assets/_Scripts/MovimentoBola.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimentoBola : MonoBehaviour{
+    [Range(1, 15)]
+    public float velocidade = 5.0f;
+    private Vector3 direcao;
+    void Start(){
+        float dirX = Random.Range(-5.0f, 5.0f);
+        float dirY = Random.Range(1.0f, 5.0f);
+        direcao = new Vector3(dirX, dirY).normalized;
+    }
+
+    // Update is called once per frame
+    void Update(){
+        transform.position += direcao * Time.deltaTime * velocidade;
+        
+        Vector2 posicaoViewport = Camera.main.WorldToViewportPoint(transform.position);
+
+        if( posicaoViewport.x < 0 || posicaoViewport.x > 1 ){
+            direcao = new Vector3(-direcao.x, direcao.y);
+        }
+        if( posicaoViewport.y < 0 || posicaoViewport.y > 1 ){
+            direcao = new Vector3(direcao.x, -direcao.y);
+        }
+    }
+   void OnTriggerEnter2D(Collider2D col){
+      float dirX = Random.Range(-5.0f, 5.0f);
+      float dirY = Random.Range(1.0f, 5.0f);
+
+      direcao = new Vector3(dirX, dirY).normalized;
+  }
+
+    
+}
---
+++ Assets/_Scripts/MovimentoRaquete.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimentoRaquete : MonoBehaviour
+{
+        [Range(1, 10)] 
+        public float velocidade;
+    // Start is called before the first frame update
+    void Start(){
+    }
+
+    // Update is called once per frame
+    void Update(){
+        float inputX = Input.GetAxis("Horizontal");
+        transform.position += new Vector3(inputX, 0, 0)* Time.deltaTime*velocidade;
+    }
+}
